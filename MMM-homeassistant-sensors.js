44902bb9a2ee46b6c93c3d6e85bf6355fefc1146
AcodeBox <AbdallahBaddour@users.noreply.github.com>
2018-04-05 13:34:12 +0300

    Change icon based on HA status
    Change icon based on HA status (on/off) - for motion sensor and lights.

--- MMM-homeassistant-sensors.js
+++ MMM-homeassistant-sensors.js
@@ -11,32 +11,32 @@ Module.register("MMM-homeassistant-sensors", {
 		displaySymbol: true,
 		values: []
 	},
-	
-	getStyles: function() {
+
+	getStyles: function () {
 		return ["modules/MMM-homeassistant-sensors/MaterialDesign-Webfont-master/css/materialdesignicons.min.css"];
 	},
-	
-	start: function() {
+
+	start: function () {
 		this.getStats();
 		this.scheduleUpdate();
 	},
-	isEmpty: function(obj) {
-		for(var key in obj) {
-			if(obj.hasOwnProperty(key)) {
+	isEmpty: function (obj) {
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key)) {
 				return false;
 			}
 		}
 		return true;
 	},
-	getDom: function() {
+	getDom: function () {
 		var wrapper = document.createElement("ticker");
 		wrapper.className = 'dimmed small';
 		var data = this.result;
-		var statElement =  document.createElement("header");
+		var statElement = document.createElement("header");
 		var title = this.config.title;
 		statElement.innerHTML = title;
 		wrapper.appendChild(statElement);
-	   
+
 		if (data && !this.isEmpty(data)) {
 			var tableElement = document.createElement("table");
 			var values = this.config.values;
@@ -51,8 +51,7 @@ Module.register("MMM-homeassistant-sensors", {
 						tableElement.appendChild(this.addValue(name, val, unit, symbol));
 					}
 				}
-			}
-			else {
+			} else {
 				for (var key in data) {
 					if (data.hasOwnProperty(key)) {
 						tableElement.appendChild(this.addValue(key, data[key], "", ""));
@@ -60,93 +59,108 @@ Module.register("MMM-homeassistant-sensors", {
 				}
 			}
 			wrapper.appendChild(tableElement);
-		} 
-		else {
+		} else {
 			var error = document.createElement("span");
 			error.innerHTML = "Error fetching stats.";
 			wrapper.appendChild(error);
 		}
 		return wrapper;
 	},
-	getValue: function(data, value) {
-		for(var i=0; i<data.length;i++){
-			if (data[i].entity_id == value){
+	getValue: function (data, value) {
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].entity_id == value) {
 				return data[i].state;
-		   }
+			}
 		}
 		return null;
 	},
-	getUnit: function(data, value) {
-		for(var i=0; i<data.length;i++){
-			if (data[i].entity_id == value){
+	getUnit: function (data, value) {
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].entity_id == value) {
 				if (typeof data[i].attributes.unit_of_measurement !== "undefined") {
 					return data[i].attributes.unit_of_measurement;
 				}
 				return "";
-		   }
+			}
 		}
 		return "";
 	},
-	getName: function(data, value) {
-		for(var i=0; i<data.length;i++){
-			if (data[i].entity_id == value){
+	getName: function (data, value) {
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].entity_id == value) {
 				return data[i].attributes.friendly_name;
 			}
 		}
 		return null;
 	},
-	addValue: function(name, value, unit, symbol) {
-		var newrow, newText, newCell;
+	addValue: function (name, value, unit, symbol) {
+		var newrow,
+		newText,
+		newCell;
 		newrow = document.createElement("tr");
 		if (this.config.stripName) {
 			var split = name.split(".");
 			name = split[split.length - 1];
 		}
 		if (this.config.prettyName) {
-			name = name.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();});
+			name = name.replace(/([A-Z])/g, function ($1) {
+					return "_" + $1.toLowerCase();
+				});
 			name = name.split("_").join(" ");
-			name = name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+			name = name.replace(/\w\S*/g, function (txt) {
+					return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+				});
 		}
 		// Symbol
 		newCell = newrow.insertCell(0);
-		newCell.className = "align-left";		
-		if (this.config.displaySymbol) {	
-			if(typeof symbol === "string") {
+		newCell.className = "align-left";
+		if (this.config.displaySymbol) {
+			if (typeof symbol === "string") {
 				var symbolinline = document.createElement("i");
-				symbolinline.className = "mdi mdi-"+symbol;
+				//Change icon based on HA status - For Motion & Light
+				if (value != "on") {
+					symbolinline.className = "mdi mdi-" + symbol;
+				} else {
+					if (name.includes('Spot') && value == "on") {
+						symbolinline.className = "mdi mdi-lightbulb-on";
+					}
+					if (name.includes('Motion') && value == "on") {
+						symbolinline.className = "mdi mdi-run-fast";
+					}
+				}
 				newCell.appendChild(symbolinline);
 			}
 		}
 		// Name
 		newCell = newrow.insertCell(1);
-		newText  = document.createTextNode(name);
+		newText = document.createTextNode(name);
 		newCell.appendChild(newText);
 		// Value
 		newCell = newrow.insertCell(2);
 		newCell.className = "align-right"
-		newText  = document.createTextNode(value);
+			newText = document.createTextNode(value);
 		newCell.appendChild(newText);
 		// Unit
 		newCell = newrow.insertCell(3);
 		newCell.className = "align-left"
-		newText  = document.createTextNode(unit);
+			newText = document.createTextNode(unit);
 		newCell.appendChild(newText);
 		return newrow;
 	},
-	scheduleUpdate: function(delay) {
+	scheduleUpdate: function (delay) {
 		var nextLoad = this.config.updateInterval;
 		if (typeof delay !== "undefined" && delay >= 0) {
 			nextLoad = delay;
 		}
 		var self = this;
-		setInterval(function() {
+		setInterval(function () {
 			self.getStats();
 		}, nextLoad);
-		},
+	},
 	getStats: function () {
 		this.sendSocketNotification('GET_STATS', this.config.url);
 	},
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function (notification, payload) {
 		if (notification === "STATS_RESULT") {
 			this.result = payload;
 			var fade = 500;
